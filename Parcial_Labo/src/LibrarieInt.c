dad98a5428e8afc2362e521b84ad0fd2c78afe64
aguinat <86366247+aguinat@users.noreply.github.com>
2021-10-14 19:32:00 -0300

    Parcial Labo

---
+++ Parcial_Labo/src/LibrarieInt.c
@@ -0,0 +1,101 @@
+#include "LibrarieValidar.h"
+#include "LibrarieInt.h"
+#include <stdlib.h>
+#include <string.h>
+
+int PedirNumeroEntero(char mensaje[], char mensajeError[],int reintentos, int* numeroEntero) {
+	char numeroIngresado[4000];
+	int validacion;
+
+	do {
+		printf("%s ", mensaje);
+		fflush(stdin);
+		scanf("%[^\n]", numeroIngresado);
+		validacion = ValidarNumero(numeroIngresado);
+		if (validacion != 1) {
+			printf("%s ", mensajeError);
+			fflush(stdin);
+			reintentos--;
+		}else{
+			*numeroEntero = atoi(numeroIngresado);
+			reintentos = 0;
+		}
+	} while (reintentos > 0);
+
+	return validacion;
+}
+
+int PedirNumeroFloat(char mensaje[], char mensajeError[],int reintentos, float* numeroFloat) {
+	char numeroIngresado[4000];
+	int validacion;
+
+	do {
+		printf("%s ", mensaje);
+		fflush(stdin);
+		scanf("%[^\n]", numeroIngresado);
+		validacion = ValidarNumeroFloat(numeroIngresado);
+		if (validacion != 1) {
+			printf("%s ", mensajeError);
+			fflush(stdin);
+			reintentos--;
+		}else{
+			*numeroFloat = atof(numeroIngresado);
+			reintentos = 0;
+		}
+	} while (reintentos > 0);
+
+	return validacion;
+}
+
+int CalcularPromedio(float numDividendo, float numDivisor, float* resultado){
+	float promedio;
+	int operacionRealizada;
+
+	operacionRealizada = 1;
+	if(numDivisor==0){
+		operacionRealizada = 0;
+	}else{
+		promedio = numDividendo / numDivisor;
+	}
+
+	*resultado = promedio;
+
+	return operacionRealizada;
+}
+
+
+int PedirNumConRango(char mensaje[], int min, int max) {
+	int numeroIngresado;
+
+	do {
+		printf("%s", mensaje);
+		scanf("%d", &numeroIngresado);
+	} while (ValidarRango(numeroIngresado, min, max) == 0);
+
+	return numeroIngresado;
+}
+
+
+int FactorialDeUnNumero(int numeroIngresado) {
+	int resultado;
+
+	if (numeroIngresado == 0) {
+		resultado = 1;
+	} else {
+		if(numeroIngresado>0){
+		resultado = numeroIngresado * FactorialDeUnNumero(numeroIngresado - 1);
+		}else{
+			resultado = -1;
+		}
+	}
+
+	return resultado;
+}
+
+void Swap(int numero1, int numero2, int *refNumero1, int *refNumero2) {
+	int aux;
+	aux = numero1;
+	*refNumero1 = numero2;
+	*refNumero2 = aux;
+}
+
---
+++ Parcial_Labo/src/LibrarieReports.c
@@ -0,0 +1,173 @@
+#include "LibrarieString.h"
+#include "LibrarieValidar.h"
+#include "LibrarieInt.h"
+#include <stdlib.h>
+#include <string.h>
+#include "LibrarieReports.h"
+
+
+void MostrarPedidosCliente(ePedido pedidos[], int tamPedidos, eCliente clientes[], int tamClientes){
+	int i;
+	int pedidosCliente;
+	int contadorClientesConPedidos;
+
+	contadorClientesConPedidos = 0;
+
+	printf("\n IdCliente \t Empresa \t  Cuit \t\tDireccion \t Localidad \t PedidosPendientes\n");
+	for (i = 0; i < tamClientes; i++) {
+		pedidosCliente = BuscarPedidosPendientes(clientes[i], tamPedidos, pedidos);
+		if(pedidosCliente != 0 && pedidos[i].isEmpty==0){
+			printf("%10d %14s %15d %15s %15s %15d\n\n", clientes[i].idCliente, clientes[i].empresa, clientes[i].cuit, clientes[i].direccion, clientes[i].entidadlocalidad.localidad, pedidosCliente);
+			contadorClientesConPedidos++;
+		}else{
+			if(contadorClientesConPedidos == 0){
+				puts("No se encontraron clientes con pedidos pendientes");
+			}
+		}
+	}
+}
+
+void MostrarPedidosPorEstado(ePedido pedidos[], int tamPedidos, eCliente clientes[], int tamClientes){
+	int i;
+	int pedidosCliente;
+	int contadorClientesConPedidos;
+
+	contadorClientesConPedidos = 0;
+
+	printf("\n Estado \t Cuit del cliente \t Dirección \t Kilos a Recolectar \n");
+	for (i = 0; i < tamClientes; i++) {
+		pedidosCliente = BuscarPedidosPendientes(clientes[i], tamPedidos, pedidos);
+		if(pedidosCliente != 0 && clientes[i].isEmpty==0){
+			printf("%10s %16d %12s %12.2f\n\n", pedidos[i].estado, clientes[i].cuit, clientes[i].direccion, pedidos[i].residuosTotales);
+			contadorClientesConPedidos++;
+		}else{
+			if(contadorClientesConPedidos == 0){
+				puts("No se encontraron clientes con pedidos pendientes");
+			}
+		}
+	}
+
+}
+
+void MostrarPedidosProcesados(ePedido pedidos[], int tamPedidos, eCliente clientes[], int tamClientes){
+	int i;
+	int pedidosCliente;
+	int contadorClientesConPedidos;
+
+	contadorClientesConPedidos = 0;
+
+	printf("\n Estado \t Cuit del cliente \t Dirección \t Residuos HDPE \t Residuos LDPE \t Residuos PP  \n");
+	for (i = 0; i < tamPedidos; i++) {
+		pedidosCliente = BuscarPedidosCompletados(clientes, tamClientes, pedidos[i]);
+		if(pedidosCliente != 0){
+			printf("%10s %16d %20s %16.2f %15.2f %15.2f\n\n", pedidos[i].estado, clientes[i].cuit, clientes[i].direccion, pedidos[i].residuosHDPE,pedidos[i].residuosLDPE, pedidos[i].residuosPP);
+			contadorClientesConPedidos++;
+		}else{
+			if(contadorClientesConPedidos == 0){
+				puts("No se encontraron clientes con pedidos completados");
+			}
+		}
+	}
+
+}
+
+void MostrarPedidosPorLocalidad(ePedido pedido[], int tamPedidos, eCliente clientes[], int tamClientes) {
+	int i;
+	int contadorPedidos;
+	int reintentos;
+	int validacionString;
+	int localidadExiste;
+	char localidad[200];
+
+	reintentos = 5;
+	contadorPedidos = 0;
+
+	validacionString = GetString("Ingrese una localidad a mostrar: ", "\nError", localidad, sizeof(localidad), reintentos);
+	if(validacionString == 1){
+		localidadExiste = BuscarLocalidad(clientes, tamClientes, localidad);
+		if(localidadExiste == 1){
+			for( i = 0; i< tamPedidos ; i++){
+				if((stricmp(pedido[i].estado, "Pendiente") == 0)){
+					for(int j= 0; j<tamClientes; j++){
+						if(clientes[j].idCliente == pedido[i].cliente && stricmp(clientes[j].entidadlocalidad.localidad, localidad)==0){
+							contadorPedidos++;
+						}
+					}
+				}
+			}
+		}
+
+	}
+
+	if(contadorPedidos!=0){
+		printf("\nPedidos pendientes: %d\n\n", contadorPedidos);
+	}else{
+		if(localidadExiste == 1){
+			puts("No se encontrados pedidos pendientes para la localidad ingresada");
+		}else{
+			puts("La localidad ingresada no existe");
+		}
+	}
+}
+
+int BuscarPedidosPendientes(eCliente cliente, int tamPedidos, ePedido pedido[]) {
+	int j;
+	int existenPedidos;
+
+	existenPedidos = 0;
+
+	for (j = 0; j < tamPedidos; j++) {
+		if (cliente.idCliente == pedido[j].cliente && (stricmp(pedido[j].estado, "Pendiente") == 0)) {
+			existenPedidos++;
+		}
+	}
+	return existenPedidos;
+}
+
+int BuscarPedidosCompletados(eCliente cliente[], int tamClientes, ePedido pedido) {
+	int j;
+	int existenPedidos;
+
+	existenPedidos = 0;
+
+	for (j = 0; j < tamClientes; j++) {
+		if (cliente[j].idCliente == pedido.cliente && (stricmp(pedido.estado,"Completado") == 0)) {
+			existenPedidos++;
+		}
+	}
+	return existenPedidos;
+}
+
+void CalcularPromedioPolipropileno(ePedido pedidos[], int tamPedidos, eCliente clientes[], int tamClientes){
+	int i ;
+	int j ;
+	eAuxliar acumulador[tamClientes];
+
+	for(i=0; i<tamClientes;i++){
+		acumulador[i].id = clientes[i].idCliente;
+		acumulador[i].acumulador = 0;
+		acumulador[i].contador = 0;
+		acumulador[i].promedio = 0;
+	}
+
+	for (i=0; i<tamClientes; i++){
+		for (j =0; j<tamPedidos; j++){
+			if(acumulador[i].id == pedidos[j].cliente){
+				acumulador[i].acumulador = acumulador[i].acumulador + pedidos[j].residuosPP;
+				acumulador[i].contador++;
+			}
+		}
+	}
+
+	printf("\n IdCliente \t PromedioPolipropileno\n");
+	for(i=0; i<tamClientes; i++){
+		acumulador[i].promedio = acumulador[i].acumulador/acumulador[i].contador;
+		if(acumulador[i].id!=0){
+			printf("%5d %20.2f\n\n", acumulador[i].id, acumulador[i].promedio);
+		}
+	}
+
+}
+
+
+
